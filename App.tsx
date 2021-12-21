f22d2c8cd1d463f033ba80b206a4aec756529b7b
Natanael Lima <taelima1997@gmail.com>
2021-12-20 23:52:09 -0300

    add keyboardviewcomponent implementation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Button,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === "ios" ? "padding" : undefined}
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.content}>
+          <StatusBar style="auto" />
+          <Text style={styles.title}>Click over form to close keyboard!</Text>
+          <ScrollView
+            contentContainerStyle={{
+              flexGrow: 1,
+            }}
+          >
+            {[...Array(20).keys()].map((i) => (
+              <TextInput style={styles.input} key={i} placeholder={String(i)} />
+            ))}
+
+            <Button title="DISMISS" onPress={Keyboard.dismiss} />
+          </ScrollView>
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingTop: Platform.OS === "ios" ? 80 : 40,
+  },
+  title: {
+    fontSize: 16,
+    paddingVertical: 16,
+    textAlign: "center",
+    fontWeight: "bold",
+    color: "rgba(0,0,0,0.6)",
+  },
+  content: {
+    flex: 1,
+  },
+  input: {
+    backgroundColor: "rgba(0,0,0,0.1)",
+    height: 40,
+    marginHorizontal: 16,
+    marginBottom: 24,
   },
 });
